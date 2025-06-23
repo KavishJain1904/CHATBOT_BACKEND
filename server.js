d869bb44da58724e9ffc48e959470b4b2aef506f
KavishJain1904 <kavishj1904@gmail.com>
2025-06-23 23:26:31 +0530

    FONT

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' data:;");
+    next();
+});
+
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 
